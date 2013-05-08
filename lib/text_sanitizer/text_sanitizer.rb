d08bdd0e4da7d453cd6959ffea86113ca2863ff7
Benjamin Larralde <benjamin.larralde@gmail.com>
2013-05-08 14:59:37 -0700

    added formatters and bump to 0.1.0

--- lib/text_sanitizer/text_sanitizer.rb
+++ lib/text_sanitizer/text_sanitizer.rb
@@ -6,6 +6,9 @@ module TextSanitizer
       self.class.send :define_method, "#{op}_text" do |*args|  # defines the class shorthand
         validate_and_set_text_fields_for op, hook, args
       end
+      self.class.send :define_method, "#{op}_formatter" do |*args|  # defines the class shorthand
+        create_formatted_attribute op, args
+      end
       send :define_method, "#{op}_text_fields" do  # defines the instance shorthand
         attrs = self.send "text_fields_to_#{op}"
         do_for_text_fields attrs, op
@@ -16,6 +19,14 @@ module TextSanitizer
     end
 
     private
+      def create_formatted_attribute op, args
+        args.map do |attr|
+          send :define_method, "formatted_#{attr}" do
+            send op, send(attr)
+          end
+        end
+      end
+
       def validate_and_set_text_fields_for op, hook, args
         attr_name = "text_fields_to_#{op}"
         self.send hook, "#{op}_text_fields"  # defines the hook
@@ -52,13 +63,13 @@ module TextSanitizer
       text.gsub(/\b\w+\b/){$&.capitalize}
     end
 
-    def convert_newlines_to_html_for text
-      paragraphs = text.split(/\r\n/)
-      paragraphs.map { |p| p.present? ? "<p>#{p}</p>" : "<br>" }.join('')
-    end
-
     def downcase text
       text.downcase
+    end
+
+    def newlines_to_html text
+      paragraphs = text.split(/\r\n/)
+      paragraphs.map { |p| p.present? ? "<p>#{p}</p>" : "<br>" }.join('')
     end
 
     def sanitize html
@@ -76,7 +87,7 @@ end
 ActiveRecord::Base.class_eval do
   include TextSanitizer
   register_sanitizer :capitalize
-  register_sanitizer :convert_newlines_to_html_for
+  register_sanitizer :newlines_to_html
   register_sanitizer :downcase
   register_sanitizer :sanitize
 end
